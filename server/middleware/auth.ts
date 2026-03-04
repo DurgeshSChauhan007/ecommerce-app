c94ac0345928ce1527d6cf5fb2a17f685e82e7d7
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2026-03-04 22:06:42 +0530

    backend completed

---
+++ server/middleware/auth.ts
@@ -0,0 +1,38 @@
+import { Response, Request, NextFunction } from "express";
+import User from "../models/User.js";
+
+export const protect = async(req: Request, res: Response, next:NextFunction) => {
+    try {
+        const { userId } = await req.auth();
+
+        if (!userId) {
+            return res.status(401).json({
+                success: false,
+                message: "Not authorized"
+            })
+        }
+
+        let user = await User.findOne({})
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log("Auth error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Authentication failed"
+        })
+    }
+}
+
+
+export const authorize = (...roles: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({
+                success: false,
+                message: "user role is not authorized to access this route."
+            })
+        }
+        next()
+    }
+}
