5abeb7e935fb78855d9e8a97545e35f63e3a8cf4
DurgeshSChauhan007 <durgeshsinghchauhan97.4@gmail.com>
2026-04-16 02:30:54 +0530

    commit updates

--- client/constants/api.ts
+++ client/constants/api.ts
@@ -1,13 +1,5 @@
 import axios from "axios";
-import { Platform } from "react-native";
 
-
-const LOCAL_API_URL = Platform.select({
-    android: "http://10.115.55.31:8081/api",
-    ios: "http://10.115.55.31:8081/api",
-    default: "http://localhost:3000/api"
-})
-
-const api = axios.create({ baseURL: LOCAL_API_URL})
+const api = axios.create({ baseURL: "https://foreverecommerce-backend.vercel.app/api"})
 
 export default api;
